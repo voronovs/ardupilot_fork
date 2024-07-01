7a1a3068820f54cf7a386ccb2374b23ba804d493
Stanislav Voronov <vrnvstanislav@yandex.ru>
2024-07-01 16:21:11 +0500

    Update RCIN_RTL_no_GPS.lua
    RCIN 8, timeout 100sec

--- libraries/AP_Scripting/my_scripts/RCIN_RTL_no_GPS.lua
+++ libraries/AP_Scripting/my_scripts/RCIN_RTL_no_GPS.lua
@@ -70,7 +70,7 @@ assert(param:add_param(PARAM_TABLE_KEY, 5, 'GPS_TRIGG_SEC', 3), 'could not add D
 
 assert(param:add_param(PARAM_TABLE_KEY, 6, 'FLY_ANGLE', 15), 'could not add DR_FLY_ANGLE param')    -- lean angle (in degrees) during deadreckoning
 assert(param:add_param(PARAM_TABLE_KEY, 7, 'FLY_ALT_MIN', 80), 'could not add DR_FLY_ALT_MIN param') -- min alt above home (in meters) during deadreckoning. zero to return at current alt
-assert(param:add_param(PARAM_TABLE_KEY, 8, 'FLY_TIMEOUT', 60), 'could not add DR_FLY_TIMEOUT param')-- deadreckoning timeout (in seconds)
+assert(param:add_param(PARAM_TABLE_KEY, 8, 'FLY_TIMEOUT', 100), 'could not add DR_FLY_TIMEOUT param')-- deadreckoning timeout (in seconds)
 assert(param:add_param(PARAM_TABLE_KEY, 9, 'NEXT_MODE', 6), 'could not add DR_NEXT_MODE param')     -- mode to switch to after GPS recovers or timeout elapses
 assert(param:add_param(PARAM_TABLE_KEY, 10, 'ENABLE_ALT', 50), 'could not add DR_ENABLE_ALT param')   -- altitude from home (in meters) beyond which the dead reckoning will be enabled
 
@@ -238,20 +238,21 @@ function update () -- periodic function that will be called
     update_user = true
   end
 
-  -- check RC failsafe
+  -- check RC
+  pwm8 = rc:get_pwm(8)
   local rc_ekf = not rc:has_valid_input()
   if rc_bad ~= rc_ekf then
     rc_bad = rc_ekf
   end
 
   -- check for RC and/or something going bad
-  if not rc_or_something_bad and rc_bad then
+  if not rc_or_something_bad and (rc_bad or pwm8 > 1600) then
     rc_or_something_bad = true
     gcs:send_text(0, "DR: RC and/or something bad")
   end
 
   -- check for RC and/or something recovery
-  if rc_or_something_bad and not rc_bad then
+  if rc_or_something_bad and (not rc_bad and pwm8 < 1600) then
     -- start recovery timer
     if recovery_start_time_ms == 0 then
       recovery_start_time_ms = now_ms
