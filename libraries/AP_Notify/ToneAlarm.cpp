5b56965751e24f6ab4a3d7930c4152f3fda1f7c9
Stas Voronov <stas.voronov@cablewalker.com>
2025-08-22 10:44:31 +0500

    change tone

--- libraries/AP_Notify/ToneAlarm.cpp
+++ libraries/AP_Notify/ToneAlarm.cpp
@@ -93,7 +93,8 @@ const AP_ToneAlarm::Tone AP_ToneAlarm::_tones[] {
 #define AP_NOTIFY_TONE_QUIET_NOT_READY_OR_NOT_FINISHED 28
     { "MFT200L4<B#4A#6G#6", false },
 #define AP_NOTIFY_TONE_STARTUP 29
-    { "MFT240L8O4aO5dcO4aO5dcO4aO5dcL16dcdcdcdc", false },
+    //{ "MFT240L8O4aO5dcO4aO5dcO4aO5dcL16dcdcdcdc", false },
+    { "MFT180L8O5eO5eO5eL4O5cO5eO5gL2O4g", false },
 #define AP_NOTIFY_TONE_NO_SDCARD 30
     { "MNBGG", false },
 #define AP_NOTIFY_TONE_EKF_ALERT 31
