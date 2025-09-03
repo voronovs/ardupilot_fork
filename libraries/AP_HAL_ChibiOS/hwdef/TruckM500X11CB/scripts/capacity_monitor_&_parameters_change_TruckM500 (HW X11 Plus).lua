c5de99c73fdfdc23bfedd43849547316f6323af9
Stas Voronov <stas.voronov@cablewalker.com>
2025-09-03 09:32:34 +0500

    upd scripts

--- libraries/AP_HAL_ChibiOS/hwdef/TruckM500X11CB/scripts/capacity_monitor_&_parameters_change_TruckM500 (HW X11 Plus).lua
+++ libraries/AP_HAL_ChibiOS/hwdef/TruckM500X11CB/scripts/capacity_monitor_&_parameters_change_TruckM500 (HW X11 Plus).lua
@@ -11,8 +11,8 @@ local change_params_flag = false
 local update_user = false -- flag of update messages to GCS
 local brd_serial_number = param:get("BRD_SERIAL_NUM")
 
-gcs:send_text(7, string.format("Serial number - %d", brd_serial_number))
-gcs:send_text(7, string.format("FW version - TruckM500_X11_v4.5.7.1"))
+gcs:send_text(7, string.format("Serial number - FLUAVTRМ500202509М%d", brd_serial_number))
+gcs:send_text(7, string.format("FW version - TruckM500_X11_CB_v4.5.7.1"))
 gcs:send_text(7, string.format("ARS version - NONE"))
 gcs:send_text(7, string.format("Parameters version - 20250903"))
 gcs:send_text(7, string.format("Script version - 20250903"))
