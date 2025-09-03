d4721fdf96e10d9a4e1e9a69b25fe8f6893a86ca
Stas Voronov <stas.voronov@cablewalker.com>
2025-09-03 08:56:09 +0500

    upd

--- libraries/AP_HAL_ChibiOS/hwdef/TruckM500X11CB/scripts/capacity_monitor_&_parameters_change_TruckM500 (HW X11 Plus).lua
+++ libraries/AP_HAL_ChibiOS/hwdef/TruckM500X11CB/scripts/capacity_monitor_&_parameters_change_TruckM500 (HW X11 Plus).lua
@@ -9,12 +9,13 @@ local state_with_payload = true
 local state_empty = false
 local change_params_flag = false
 local update_user = false -- flag of update messages to GCS
+local brd_serial_number = param:get("BRD_SERIAL_NUM")
 
-gcs:send_text(7, string.format("Serial number - 1"))
-gcs:send_text(7, string.format("FW version - TruckM500_X11_CubeBlack_v4.5.7.1"))
+gcs:send_text(7, string.format("Serial number - %d", brd_serial_number))
+gcs:send_text(7, string.format("FW version - TruckM500_X11_v4.5.7.1"))
 gcs:send_text(7, string.format("ARS version - NONE"))
-gcs:send_text(7, string.format("Parameters version - 20250820"))
-gcs:send_text(7, string.format("Script version - 20250820"))
+gcs:send_text(7, string.format("Parameters version - 20250903"))
+gcs:send_text(7, string.format("Script version - 20250903"))
 
 function updateParameters ()
     if state_with_payload and change_params_flag then
