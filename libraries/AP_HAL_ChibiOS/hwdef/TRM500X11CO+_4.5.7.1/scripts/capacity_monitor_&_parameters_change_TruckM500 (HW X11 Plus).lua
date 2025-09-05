8c0d5ded81968e9216f94419f5a92da6b11eb84f
Stas Voronov <stas.voronov@cablewalker.com>
2025-09-05 11:19:27 +0500

    upd TRM500X11xx_4.5.7.1

--- libraries/AP_HAL_ChibiOS/hwdef/TRM500X11CO+_4.5.7.1/scripts/capacity_monitor_&_parameters_change_TruckM500 (HW X11 Plus).lua
+++ libraries/AP_HAL_ChibiOS/hwdef/TRM500X11CO+_4.5.7.1/scripts/capacity_monitor_&_parameters_change_TruckM500 (HW X11 Plus).lua
@@ -14,7 +14,7 @@ local brd_serial_number = param:get("BRD_SERIAL_NUM")
 gcs:send_text(7, string.format("Serial number - FLUAVTRМ500202509М%d", brd_serial_number))
 gcs:send_text(7, string.format("FW version - TRM500X11CO+_4.5.7.1"))
 gcs:send_text(7, string.format("ARS version - NONE"))
-gcs:send_text(7, string.format("Parameters version - 20250903"))
+gcs:send_text(7, string.format("Parameters version - 20250905"))
 gcs:send_text(7, string.format("Script version - 20250903"))
 
 function updateParameters ()
