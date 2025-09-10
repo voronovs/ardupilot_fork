888f85ffb8278b5c3180e453f8d5fda57b9af839
Stas Voronov <stas.voronov@cablewalker.com>
2025-09-10 10:29:19 +0500

    upd TRM500X11CO+_4.5.7.2

--- libraries/AP_HAL_ChibiOS/hwdef/TRM500X11CO+/scripts/capacity_monitor_&_parameters_change_TruckM500 (HW X11 Plus).lua
+++ libraries/AP_HAL_ChibiOS/hwdef/TRM500X11CO+/scripts/capacity_monitor_&_parameters_change_TruckM500 (HW X11 Plus).lua
@@ -12,10 +12,10 @@ local update_user = false -- flag of update messages to GCS
 local brd_serial_number = param:get("BRD_SERIAL_NUM")
 
 gcs:send_text(7, string.format("Serial number - FLUAVTRМ500202509М%d", brd_serial_number))
-gcs:send_text(7, string.format("FW version - TRM500X11CO+_4.5.7.1"))
+gcs:send_text(7, string.format("FW version - TRM500X11CO+_4.5.7.2"))
 gcs:send_text(7, string.format("ARS version - NONE"))
-gcs:send_text(7, string.format("Parameters version - 20250905"))
-gcs:send_text(7, string.format("Script version - 20250903"))
+gcs:send_text(7, string.format("Parameters version - 20250910"))
+gcs:send_text(7, string.format("Script version - 20250910"))
 
 function updateParameters ()
     if state_with_payload and change_params_flag then
