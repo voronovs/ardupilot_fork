3d8c3686378eb9d999c86267d5c31d9fedc437ed
Stas Voronov <stas.voronov@cablewalker.com>
2025-09-19 17:32:19 +0500

    Updates in TRM500X11xxx_4.5.7.4: > Для ZT6 MNT1_TYPE, 0 (удален из defaults.parm в прошивке) > Исправление скрипта: Смена параметров Empty/Payload в полете происходит на основании средней потребляемой мощности (больше или меньше 3100W) за 5сек. До этого скрипт работал некорректно и измерял значение каждую секунду.

--- libraries/AP_HAL_ChibiOS/hwdef/TRM500X11CO+/scripts/capacity_monitor_&_parameters_change_TruckM500 (HW X11 Plus).lua
+++ libraries/AP_HAL_ChibiOS/hwdef/TRM500X11CO+/scripts/capacity_monitor_&_parameters_change_TruckM500 (HW X11 Plus).lua
@@ -12,13 +12,13 @@ local update_user = false -- flag of update messages to GCS
 local brd_serial_number = param:get("BRD_SERIAL_NUM")
 local watts_sum = 0
 local sample_count = 0
-local avg_watts = 0 -- средняя мощность
+local avg_watts = 0 -- average power consumption
 
-gcs:send_text(7, string.format("Serial number - FLUAVTRМ500202509М%d", brd_serial_number))
-gcs:send_text(7, string.format("FW version - TRM500X11CO+_4.5.7.3"))
+gcs:send_text(7, string.format("Serial number - FLUAVTRМ500202509М%d", brd_serial_number)) -- вставить серийный номер
+gcs:send_text(7, string.format("FW version - TRM500X11CO+_4.5.7.4")) -- вставить название полетного контроллера
 gcs:send_text(7, string.format("ARS version - NONE"))
-gcs:send_text(7, string.format("Parameters version - 20250910"))
-gcs:send_text(7, string.format("Script version - 20250917"))
+gcs:send_text(7, string.format("Parameters version - 20250919"))
+gcs:send_text(7, string.format("Script version - 20250919"))
 
 function updateParameters ()
     if state_with_payload and change_params_flag then
@@ -79,12 +79,12 @@ function updateBatteryInfo()
 
         avg_watts = watts_sum / sample_count
 
-        if math.floor(avg_watts) > 3600 and state_empty then
+        if math.floor(avg_watts) > 3100 and state_empty then
             change_params_flag = true
             state_with_payload = true
             state_empty = false
             updateParameters()
-        elseif math.floor(avg_watts) <= 3600 and state_with_payload then
+        elseif math.floor(avg_watts) <= 3100 and state_with_payload then
             change_params_flag = true
             state_with_payload = false
             state_empty = true
@@ -100,6 +100,7 @@ end
 -- main
 function update ()
     local now_ms = millis() -- get the time since boot
+    update_user = false
 
     if (now_ms - last_print_ms > 5000) then -- update every 5s, ms
         last_print_ms = now_ms
